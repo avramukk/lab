f633cd834925ab51195b5f94441be6853f783992
avramukk <avramuk89@gmail.com>
2024-05-20 14:28:03 +0100

    playing with rust

---
+++ rust/src/main.rs
@@ -0,0 +1,12 @@
+#![allow(unused_variables, unused_assignments)]
+
+fn main() {
+    let mut number = 3;
+
+    while number !=  {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+
+}
