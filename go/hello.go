55a2e5c901ddb67a892b693da56e174c73ccb93e
avramukk <avramuk89@gmail.com>
2024-06-05 14:18:37 +0100

    go variables

--- go/hello.go
+++ go/hello.go
@@ -3,7 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var n bool = true
-	fmt.Print("%v, %T\n", n, n)
-
+    fmt.Println("hello world")
 }
---
+++ go/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Змінні
+    var name string = "Kolia"
+    var age int = 28
+    var isStudent bool = false
+		var isEngineer bool = true
+
+    // Виведення значень змінних
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Is Student:", isStudent)
+		fmt.Println("Is Engineer:", isEngineer)
+}
